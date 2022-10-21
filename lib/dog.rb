4c1e3d3bbb606077f70452adb2436aed0078f629
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-10-21 11:54:02 +0300

    Completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Dog
+# class body
+
+def bark
+    puts "Woof!"
+  end 
+def sit 
+    puts "The Dog is sitting"
+
+ end
+end
+
+fido = Dog.new
+fido.bark
+
+fido = Dog.new
+fido.sit 
